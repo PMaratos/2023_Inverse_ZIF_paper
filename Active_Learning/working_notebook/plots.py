5290ecfa66402b616bc59cba7b4b28d963a16fff
vGkatsis <vgkatsis@gmail.com>
2025-03-11 17:16:42 +0200

    Added functionality for plotting the results of Real datasets.

--- Active_Learning/working_notebook/plots.py
+++ Active_Learning/working_notebook/plots.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -39,29 +40,51 @@ def stat_test(df1,df2):
 
 if __name__ == "__main__":
 
-    al_selection_methods = ["density", "igs", "qbc", "rt", "random"]
-    dataset_names = ["Forrester", "forrester_imb", "jump_forrester", "jump_forrester_imb", "gaussian", "gaussian_imb", "gaussian_imb_noise", "exponential", "exponential_imb"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", '--type', help="The type of data to plot, either real or synthetic.", default='synthetic')
+    parser.add_argument("-d", '--data', help="The the path to the dataset.",                        default="./ALresults/Synthetic/")
 
-    dataset_to_plot_names = {"Forrester"          : "Forrester", 
-                             "forrester_imb"      : "Forrester Imbalanced", 
-                             "jump_forrester"     : "Jump Forrester", 
-                             "jump_forrester_imb" : "Jump Forrester Imbalanced", 
-                             "gaussian"           : "Gaussian", 
-                             "gaussian_imb"       : "Gaussian Imbalanced", 
-                             "gaussian_imb_noise" : "Gaussian Imbalanced with Noise", 
-                             "exponential"        : "Exponential", 
-                             "exponential_imb"    : "Exponential Imbalanced"}
+    args = parser.parse_args()
+
+    data_type = args.type
+    data_path = args.data
+
+    al_selection_methods = ["density", "igs", "qbc", "rt", "random"]
+
+    dataset_names = None
+    dataset_to_plot_names = None
+    if data_type == "real":
+        dataset_names = ["CH4", "H2", "He", "N2", "O2"]
+
+        dataset_to_plot_names = {}
+        for name in dataset_names:
+            dataset_to_plot_names[name] = name
+
+        data_path = './ALresults/Real/'
+
+    else:
+        dataset_names = ["Forrester", "forrester_imb", "jump_forrester", "jump_forrester_imb", "gaussian", "gaussian_imb", "gaussian_imb_noise", "exponential", "exponential_imb"]
+
+        dataset_to_plot_names = {"Forrester"          : "Forrester", 
+                                 "forrester_imb"      : "Forrester Heterogeneous", 
+                                 "jump_forrester"     : "Jump Forrester", 
+                                 "jump_forrester_imb" : "Jump Forrester Heterogeneous", 
+                                 "gaussian"           : "Gaussian", 
+                                 "gaussian_imb"       : "Gaussian Heterogeneous", 
+                                 "gaussian_imb_noise" : "Gaussian Heterogeneous with Noise", 
+                                 "exponential"        : "Exponential", 
+                                 "exponential_imb"    : "Exponential Heterogeneous"}
 
     experiment_results = []
     for dataset in dataset_names:
         for method in al_selection_methods:
-            experiment_results.append(create_results('./ALresults/Synthetic/' + method + '/' + dataset + '/' + dataset + '_150_' + method))
+            experiment_results.append(create_results(data_path + method + '/' + dataset + '/' + dataset + '_150_' + method))
 
 
         t_v, p_v = stat_test(experiment_results[0],experiment_results[1])
         p_v = p_v.item()
 
-        f_size = 16
+        f_size = 13
         linewidth = 2.2
 
         plt.figure()
@@ -81,7 +104,7 @@ if __name__ == "__main__":
         handles, labels = plt.gca().get_legend_handles_labels()
         plt.legend(handles, labels, fontsize=f_size)
 
-        save_path = os.curdir + '/plots'
+        save_path = os.path.join(os.curdir, 'plots', data_type)
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
